52ceda17e5931c2ff18a8c0ecc434c592d780a42
komal10vishwakarma <149768673+komal10vishwakarma@users.noreply.github.com>
2026-09-10 20:55:12 +0530

    hi

--- python-assignment/function/menu-driven-day2/pro1.py
+++ python-assignment/function/menu-driven-day2/pro1.py
@@ -17,11 +17,12 @@ def fact(n):
 	return fact
 
 def s(n):
+	num=str(n)
 	sum=0
-	for i in range(0,n+1):
-		sum=sum+i
+	for i in num:
+		sum=sum+int(i)
 	print("the value of sum is:",sum)
-	return s
+	return sum
 
 def nod(n):
 	x=str(n)
@@ -44,7 +45,84 @@ def pal(n):
 		print("Number is not palindrome")
 		
 	return pal
+
+
+def automor(n):
+	square=n*n
+	print(square)
+	st=str(square)
+	i=len(str(n))
+
+	lastnumber=st[-i:]	
+	print(lastnumber)
+	if n==int(lastnumber):
+		print("Automorphic Number")
+	else:
+		print("Not an Automorphic Number")
+	return automor
 	
+def neon(n):	
+	original=n
+	square=n*n
+	inq=str(square)
+	sum=0
+	for i in inq:
+		sum=sum+int(i)
+	print(sum)
+	if original==sum:
+		print("Neon Number")
+	else:
+		print("Not Neon Number")
+	return neon 
+
+	
+def spy(n):
+	add=s(n)
+	print(add)
+	num=str(n)
+	pro=1
+	for i in num:
+		pro=pro*int(i)
+	print(pro)
+	if add==pro:
+		print("Spy Number")
+	else:
+		print("Not Spy Number")
+	return spy	
+	
+		
+def harshad(n):
+	original=n
+	add=s(n)
+	print(n)
+	if original % add == 0:
+		print("Harshad Number")
+	else:
+		print(" Not Harshad Number")
+	
+	return harshad
+
+def perfect_number(n):
+	sum = 0
+	for i in range(1, n):
+		if n % i == 0:
+			sum = sum + i
+	if sum == n:
+			print(n, "is a Perfect Number")
+	else:
+			print(n, "is not a Perfect Number")
+
+	
+
+	
+def prime(n):
+	if n%2==0:
+		print("Prime Number")
+	else:
+		print("Not Prime Number")
+	
+
+
 
 while True:
 	print("""
@@ -66,6 +144,21 @@ while True:
 	""")
 	choice=int(input("enter your choice here:"))	
 	match choice:
+		case 1:
+			n = int(input("Enter a number: "))
+			perfect_number(n)
+		case 2:
+			n=int(input("enter your number:"))
+			pal(n)
+
+		case 3:
+			pass
+		case 4:
+			n = int(input("Enter a number: "))
+			prime(n)
+		case 5:
+			n=int(input("enter your number:"))
+			prime(n)
 		case 6:
 			n=int(input("enter your number:"))
 			if eo(n):
@@ -78,15 +171,29 @@ while True:
 		case 8:
 			n=int(input("enter your number:"))
 			s(n)
-		case 10:
-			n=int(input("enter your number:"))
-			nod(n)
 		case 9:
 			n=int(input("enter your number:"))
 			rev(n)
-		case 2:
+		case 10:
 			n=int(input("enter your number:"))
-			pal(n)
+			nod(n)
+		case 11:
+			n=int(input("enter your number:"))
+			automor(n)
+		case 12:
+			n=int(input("enter your number:"))
+			neon(n)
+		case 13:
+			n=int(input("enter your number:"))
+			spy(n)
+		case 14:
+			n=int(input("enter your number:"))
+			harshad(n)
+		case 15:
+			print("exit")
+		
+		case _:
+			print("You are out of loop")
 			
 			
 			
---
+++ python-assignment/modules/pro1.py
@@ -0,0 +1,17 @@
+from datetime import datetime,timedelta
+
+date=input("Enter DOB (DD-MM-YYYY):")
+date=datetime.strptime(date,"%d-%m-%Y")
+print(date)
+
+today=date.today()
+
+y1=date.year
+y2=today.year
+age=y2-y1
+print("current age is:",age,"years")
+
+nextbirthday=date+timedelta(year=1)
+print(nextbirthday)
+
+
